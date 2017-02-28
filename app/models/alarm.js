7972cd697d55b68f4ca5744dbc4b068767162030
jens <jens@lea.io>
2017-02-28 19:22:47 +0100

    Alarm time is string

--- app/models/alarm.js
+++ app/models/alarm.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     title: DS.attr('string'),
-    time: DS.attr('datetime'),
+    time: DS.attr('string'),
     on: DS.attr('boolean'),
     repeat: DS.attr('boolean')
 });
